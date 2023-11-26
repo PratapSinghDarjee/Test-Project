eb59ab65c87a1aa265c3a5e16961905213f87008
Pratap Singh Darjee <Dell@DESKTOP-M2SUBJ9>
2023-11-26 12:15:00 -0500

    Added programmer comments, and starter code to method main

---
+++ Assignment03/src/Assignment03/Assignment03PratapSinghDarjee.java
@@ -0,0 +1,24 @@
+package Assignment03;
+public class Assignment03PratapSinghDarjee {
+
+	public static void main(String[] args) {
+		/*int roll = 0;
+		int[] diceRolls = new int[20];
+		int totalRolls = 0;
+
+		// sample the rolls
+		for(int count = 0; count <=20; count++) {
+		    roll = (int)(Math.random() * 20) + 1;
+		    diceRolls[roll] = diceRolls[roll - 1] + 1; // crashes
+		}	
+		// run a report
+		for(int index = 0; index < diceRolls.length; index++) {
+		    System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+		    totalRolls = totalRolls + diceRolls[index];
+		}
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("Program by Your Name");*/
+		System.out.println("Pratap Singh Darjee");
+	}
+
+}
